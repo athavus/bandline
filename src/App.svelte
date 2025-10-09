13df7ae852615aa1f6cdc6da489284647e398109
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2025-10-09 00:13:11 -0300

    initial commit: testando svelte

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let count:number = 0;
+
+
+  function aumentarContador() {
+    count++;
+  }
+</script>
+
+<main>
+  <button class="buttons" on:click={aumentarContador}>
+    O número no contador é {count}
+  </button>
+</main>
+
+<style>
+  .buttons {
+    border: none;
+    outline: none;
+  }
+</style>
